20c3603ea4885baacf60aa9053690cfb0ed2fc3a
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-09-29 21:28:48 +0200

    Add devtools, fixed store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+
 import weather from './reducers/weather';
 import DevTools from './DevTools/DevTools'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 export const store = createStore(
     weather,
-    composeEnhancer(
+    compose(
         applyMiddleware(thunk),
+        DevTools.instrument()
+
     )
 );
