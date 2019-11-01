d35ad6507efbc62bbeb95bb0a20a89b05b5aaeb5
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-11-02 00:23:38 +0100

    Fixed routes and basic structure

---
+++ src/modules/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Router, Route, browserHistory } from 'react-router'
+
+import CityInputContainer from './Containers/cityInput.container'
+import DevTools from '../redux/DevTools/DevTools';
+import TemperatureContainer from './Containers/Temperature.container';
+import GeneralContainer from './Containers/General.container';
+
+export default class IndexModules extends React.Component {
+
+    render() {
+        return (
+            <div className='App'>
+                <main className='container'>
+                    <CityInputContainer />
+                    <GeneralContainer />
+                    <TemperatureContainer />
+                </main>
+            </div>
+        )
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,10 @@
 import React from "react";
 import { Router, Route, browserHistory } from 'react-router'
 
-import CityInputContainer from './modules/Containers/cityInput.container'
-import DevTools from './redux/DevTools/DevTools';
-import TemperatureContainer from './modules/Containers/Temperature.container';
-import GeneralContainer from './modules/Containers/General.container';
-import Home from './modules/Home/Home';
+import IndexModules from './modules/index';
 
-export const AppRoutes = () => 
+export const AppRoutes = () =>
 
-<Router history={browserHistory}>
-    <div className='App'>
-
-        {/* <main className='container'> */}
-        <Route exact path="/" component={Home} />
-        <Route path='/weather-info' component={CityInputContainer} />
-        {/* <CityInputContainer />
-          <GeneralContainer />
-          <TemperatureContainer />
-          <DevTools /> */}
-
-        {/* </main> */}
-    </div>
-</Router>
+    <Router history={browserHistory}>
+        <Route exact path="/" component={IndexModules} />
+    </Router>
