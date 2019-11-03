4337a9d74db6f7bf00d9b3dbcbad3d0e238a624a
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-11-03 19:30:50 +0100

    Updates

---
+++ precache-manifest.946ebdf5f68767019ce5c37e5ac93f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5addeab9db3fab419d715d0c9e438ba",
+    "url": "/React-Weather/index.html"
+  },
+  {
+    "revision": "b7f4ce6af04facd20aaf",
+    "url": "/React-Weather/static/css/main.e4f2d240.chunk.css"
+  },
+  {
+    "revision": "d035aee98486760d6eb3",
+    "url": "/React-Weather/static/js/2.78cd164c.chunk.js"
+  },
+  {
+    "revision": "b7f4ce6af04facd20aaf",
+    "url": "/React-Weather/static/js/main.cf53d6b7.chunk.js"
+  },
+  {
+    "revision": "dd998ef2fdee92a8388e",
+    "url": "/React-Weather/static/js/runtime~main.37f73410.js"
+  },
+  {
+    "revision": "00daa64da825f3bfb7630289707ead02",
+    "url": "/React-Weather/static/media/sky.00daa64d.jpg"
+  }
+]);
